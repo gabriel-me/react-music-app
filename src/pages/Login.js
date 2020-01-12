a703619df0568284ad0308f207f1da6a5893029e
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2020-01-12 17:36:11 -0300

    Add login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,3 +1,62 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
+import styled from 'styled-components'
+import * as yup from 'yup'
 
-export default () => <h1>Login</h1>
+import { login } from '../auth'
+import Button from '../components/Button'
+import Container from '../components/Container'
+import { Form, Field } from '../components/Form'
+
+const LoginPage = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+`
+
+const Title = styled.h1`
+  text-align: center;
+  padding: 1rem 0;
+`
+
+export default () => {
+  const history = useHistory()
+
+  const fieldInitialValues = {
+    email: '',
+    password: ''
+  }  
+
+  const fieldValidations = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().min(8).required()
+  })
+
+  const handleSignIn = data => {
+    const { email, password } = data
+
+    if (email && password) {
+      login('tokenapi')
+      history.push('/')
+    }
+  }
+
+  return (
+    <LoginPage>
+      <Container width='30rem'>
+        <Title>Todo APP</Title>
+        <Form 
+          initialValues={fieldInitialValues} 
+          validationSchema={fieldValidations}
+          onSubmit={handleSignIn}
+        >
+          <Field name='email'/>
+          <Field name='password'/>
+          <Button type='primary' width='100'>Gravar</Button>
+        </Form>
+      </Container>
+    </LoginPage>
+  )
+}
